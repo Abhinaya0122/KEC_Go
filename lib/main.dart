b899a45592fc14704c8475f7909f5219745b8155
Abhinaya0122 <abhinayaps.22cse@kongu.edu>
2024-09-26 09:08:59 +0530

    Feat:initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './pages/bottom_nav.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BottomNav(),
+    );
+  }
+}
